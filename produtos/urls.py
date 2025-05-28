61dd81dc76c1a04e00c16ace8f3e708fe8001d9e
Romulo Sousa <romulo22.dev@gmail.com>
2025-05-28 01:08:49 -0300

    tabalhoFinal

--- produtos/urls.py
+++ produtos/urls.py
@@ -10,13 +10,11 @@ urlpatterns = [
     path('detalhe/<int:pk>/', views.detalhe_produto, name='detalhe_produto'),
 
     # Clientes
-    # path('', views.lista_produtos, name='lista_produtos'),
     path('novo_cliente/', views.cria_cliente, name='cria_cliente'),
     path('edita_cliente/<int:pk>/', views.edita_cliente, name='edita_cliente'),
     path('remove_cliente/<int:pk>/', views.remove_cliente, name='remove_cliente'),
 
     # Vendas
-    # path('', views.lista_produtos, name='lista_produtos'),
     path('novo_venda/', views.cria_venda, name='cria_venda'),
     path('edita_venda/<int:pk>/', views.edita_venda, name='edita_venda'),
     path('remove_venda/<int:pk>/', views.remove_venda, name='remove_venda'),
